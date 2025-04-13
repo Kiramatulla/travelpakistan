35c8bbea701f6e8cd9cf25839474c7eb1363daed
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-04-13 21:17:03 +0500

    Trek Page Designed
    Trekking Page Designed

--- src/app/trekking/page.jsx
+++ src/app/trekking/page.jsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { client } from "@/sanity/lib/client";
+import TrekPagCard from "../components/trekingComponents/TrekPagCard";
 
-const page = () => {
+
+const page = async() => {
+  const treks = await client.fetch(`*[_type == "treks"]`);
   return (
-    <div>Trekking page</div>
+   <section>
+    <p className="text-3xl font-bold mt-10 text-center underline font-sans">
+        ALL OF OUR TREKS
+      </p>
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 lg:mx-32 mt-6 gap-4">
+        {treks.map((trek, index) => (
+          <TrekPagCard key={index} trek={trek} />
+        ))}
+      </div>
+   </section>
   )
 }
 
