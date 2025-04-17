592a346095917afa7909c669b099afb5e2d34a99
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-04-18 03:02:44 +0500

    More editing done to tour detail page

--- src/app/components/tourDetailPage/Overview.jsx
+++ src/app/components/tourDetailPage/Overview.jsx
@@ -1,10 +1,11 @@
 const OverView = ({ tours }) => {
     return (
       <section className="px-6 text-justify lg:text-start mt-12 mb-8 lg:pl-8 lg:pr-8">
-        <h1 className="font-bold text-2xl font-sans mb-4">{tours.title}</h1>
-        <p className=" font-sans lg:text-sm lg:leading-6">{tours.tourOverview}</p>
+        <h1 className="font-bold text-2xl text-gray-800 font-sans mb-4">{tours.title}</h1>
+        <p className=" font-sans text-sm lg:leading-7">{tours.tourOverview}</p>
       </section>
     );
   };
   
   export default OverView;
+
--- src/app/components/tourDetailPage/UserGuide.jsx
+++ src/app/components/tourDetailPage/UserGuide.jsx
@@ -5,7 +5,7 @@ const UserGuide = () => {
     "We offer 4 options based on the category of hotel and services: Premium, Mid Range, Standard, and Basic.",
     "The price mentioned in the 'Price Per Person' section is for Standard Services. For other options, please contact our agents.",
     "The price per person decreases as the number of participants increases.",
-    "For history buffs, a Hunza Valley tour promises an unforgettable experience — from hiking to the stunning Hopper Glacier.",
+    "The side section(when viewed on laptop) provides additional information about the regions histroy, culture and top tourist hotspots in the region.",
   ];
 
   return (
