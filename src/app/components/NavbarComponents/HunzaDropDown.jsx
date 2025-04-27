7bfff40eb6b7ac1e02320e713a7b9a080637b551
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-04-28 00:36:28 +0500

    car image updated

--- src/app/components/NavbarComponents/HunzaDropDown.jsx
+++ src/app/components/NavbarComponents/HunzaDropDown.jsx
@@ -66,7 +66,7 @@ const HunzaDropDown = () => {
         href="#"
         className="block px-4 py-2 hover:bg-cyan-950 hover:text-white"
       >
-        Karachi One Day
+        Karachi One Day Tour
       </Link>
     </div>
   </div>
