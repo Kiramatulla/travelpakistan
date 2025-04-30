31ec6bbbf77e5b92ecd64171b9ce6b6494ff6727
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-04-30 15:06:50 +0500

    changed

--- src/app/tours/[slug]/page.jsx
+++ src/app/tours/[slug]/page.jsx
@@ -1,18 +1,7 @@
 import TourDetailsPage from "@/app/components/tourDetailPage/TourDetailsPage";
 import { client } from "@/sanity/lib/client";
 
-// export const dynamicParams = true;
-// export const revalidate = 0;
 
-// ✅ This will generate static pages for each tour at build time
-export async function generateStaticParams() {
-  const query = `*[_type == "tour"]{ "slug": slug.current }`;
-  const tours = await client.fetch(query);
-
-  return tours.map((tour) => ({
-    slug: tour.slug,
-  }));
-}
 
 export async function generateMetadata(props) {
   const params = await props.params;
