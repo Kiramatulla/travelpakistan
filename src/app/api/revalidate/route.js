0d575e3cae1ba3d1867b91b9dca6d181763ab185
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-04-30 15:01:47 +0500

    revalidate

--- src/app/api/revalidate/route.js
+++ src/app/api/revalidate/route.js
@@ -1,22 +1,16 @@
-import { revalidatePath } from 'next/cache';
-
-
+import { revalidatePath } from 'next/cache'
+ 
 export async function GET(request) {
-  const path = request.nextUrl.searchParams.get('path');
-  
-
-  if (secret !== process.env.REVALIDATE_SECRET) {
-    return Response.json({ message: 'Invalid secret' }, { status: 401 });
-  }
-
+  const path = request.nextUrl.searchParams.get('path')
+ 
   if (path) {
-    revalidatePath(path);
-    return Response.json({ revalidated: true, now: Date.now() });
+    revalidatePath(path)
+    return Response.json({ revalidated: true, now: Date.now() })
   }
-
+ 
   return Response.json({
     revalidated: false,
     now: Date.now(),
     message: 'Missing path to revalidate',
-  });
+  })
 }
