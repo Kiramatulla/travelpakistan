4b05a4d4ec578a806dd375447b3bf4acfb73c15d
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-05-08 13:28:55 +0500

    blog link updated

--- src/app/components/blogComponents/BlogImageCard.jsx
+++ src/app/components/blogComponents/BlogImageCard.jsx
@@ -5,7 +5,7 @@ import { FaArrowRightLong } from "react-icons/fa6";
 
 const BlogImageCard = ({ blog }) => {
   return (
-    <Link href={`/blogs/${blog.slug.current}`}>
+    
       <article className="w-full mx-auto mb-8 md:mb-10 transition-transform transform hover:-translate-y-1 hover:shadow-lg overflow-hidden bg-white">
         <div className="">
           <Image
@@ -21,13 +21,15 @@ const BlogImageCard = ({ blog }) => {
           <h3 className="font-semibold text-slate-700 mb-2 line-clamp-2">
             {blog.title}
           </h3>
+          <Link href={`/blogs/${blog.slug.current}`}>
           <p className="text-sm text-cyan-700 hover:text-orange-500 font-medium inline-flex items-center group">
             Read Post
             <FaArrowRightLong className="ml-1 mt-[2px] transition-transform duration-150 group-hover:translate-x-1" />
           </p>
+          </Link>
         </div>
       </article>
-    </Link>
+   
   );
 };
 
