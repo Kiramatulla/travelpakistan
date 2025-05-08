cb0d4723e3d960cd7d26eaca54b110c4a54ff9f6
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-05-08 11:46:38 +0500

    trekking section updated

--- src/app/components/blogComponents/BlogCard.jsx
+++ src/app/components/blogComponents/BlogCard.jsx
@@ -1,33 +1,12 @@
-import { client } from "@/sanity/lib/client";
-import BlogImageCard from "./BlogImageCard";
+// import { client } from "@/sanity/lib/client";
+// import BlogImageCard from "./BlogImageCard";
 
-const BlogCards = async () => {
-  const blogs = await client.fetch(`*[_type == "blogs"]`);
+// const BlogCards = async () => {
+//   const blogs = await client.fetch(`*[_type == "blogs"]`);
 
-  return (
-    <div className="py-12 lg:py-12 font-sans bg-white">
-      <div className="max-w-7xl mx-auto px-4">
-        {/* Header */}
-        <header className="text-center mb-12">
-          <h2 className="text-xl sm:text-4xl md:text-5xl font-bold text-slate-800 mb-4">
-            Our Recent Blogs
-          </h2>
-          <p className="text-gray-600 text-base md:text-sm max-w-2xl mx-auto">
-            Learn more about your favorite destinations in our blog section —
-            from history and tourist attractions to local activities, travel distances,
-            and drive times. We have got everything covered.
-          </p>
-        </header>
+//   return (
+    
+//   );
+// };
 
-        {/* Blog Cards */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6">
-          {blogs?.map((blog, index) => (
-            <BlogImageCard blog={blog} key={index} />
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default BlogCards;
+// export default BlogCards;
--- src/app/trekking/page.jsx
+++ src/app/trekking/page.jsx
@@ -1,16 +1,17 @@
 import Link from "next/link";
 import TrekPagCard from "../components/trekingComponents/TrekPagCard";
 import { client } from "@/sanity/lib/client";
+import { IoSearchOutline } from "react-icons/io5";
 
-const PAGE_SIZE = 12; 
+const PAGE_SIZE = 12;
 
-export const metadata ={
-  title:"Trekking in Pakistan, K2 Basecamp, Gondogoro La",
-  description:"Looking for a once-in-a-lifetime adventure? Discover 20+ handpicked Pakistan tour packages for 2025, designed just for you. Whether you want to explore the mountains of Hunza, the deserts of Cholistan, or the culture of Lahore — weve got a trip that fits your style, budget, and time",
-  
-}
+export const metadata = {
+  title: "Trekking in Pakistan, K2 Basecamp, Gondogoro La",
+  description:
+    "Looking for a once-in-a-lifetime adventure? Discover 20+ handpicked Pakistan tour packages for 2025, designed just for you. Whether you want to explore the mountains of Hunza, the deserts of Cholistan, or the culture of Lahore — weve got a trip that fits your style, budget, and time",
+};
 
-const Page = async props => {
+const Page = async (props) => {
   const searchParams = await props.searchParams;
   const currentPage = parseInt(searchParams.page || "1");
   const start = (currentPage - 1) * PAGE_SIZE;
@@ -22,12 +23,38 @@ const Page = async props => {
   const treks = await client.fetch(`*[_type == "treks"][${start}...${end}]`);
 
   return (
-    <main>
-      <p className="text-3xl font-bold mt-10 text-center underline font-sans">
-        ALL OF OUR TREKS
-      </p>
-      
-      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 lg:mx-32 mt-6 gap-4">
+    <main className="font-sans flex flex-col items-center">
+      <header className="mx-4 max-w-screen-md text-center">
+        <h1 className="text-3xl text-slate-600 font-bold mt-8 mb-2 md:mb-2">
+          Trekking the Karakoram, Himalaya and Hindu Kush, 2025
+        </h1>
+        <p className=" text-gray-500 text-center mb-8">
+         We offer several trekking options in Pakistan, our trekking covers
+         from the mighty K2 Base camp to Gondogora La, we also cover hidden passes
+         including Khurdupin pass, Hisper La, Lupke La and Snow lake Trek.
+         Find everything you are looking below:
+        </p>
+
+        <div>
+          <form
+            action="/search"
+            method="GET"
+            className="flex items-center space-x-4 border-2 bg-white rounded-full shadow-lg overflow-hidden w-full max-w-md mx-auto"
+          >
+            <input
+              type="text"
+              name="query"
+              placeholder="Search..."
+              className="py-6 px-4 text-black outline-none w-full"
+            />
+            <button type="submit" className="text-black pr-4 py-2 rounded-full">
+              <IoSearchOutline size={20} />
+            </button>
+          </form>
+        </div>
+      </header>
+
+      <section className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 mt-14 lg:mx-20 md:gap-4">
         {treks.map((trek, index) => (
           <TrekPagCard key={index} trek={trek} />
         ))}
@@ -72,6 +99,3 @@ const Page = async props => {
 };
 
 export default Page;
-
-
-
