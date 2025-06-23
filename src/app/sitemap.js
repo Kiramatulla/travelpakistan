6f8c26d22798cd6defb9f48eeb7f075e0629a4a9
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-06-23 22:00:14 +0500

    sitemap added

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,17 +1,26 @@
-import { client } from '@/sanity/lib/client'
+import { client } from '@/sanity/lib/client';
 
 const sitemap = async () => {
   const posts = await client.fetch(`*[_type == "tour" || _type == "treks" || _type == "blogs"]{
+    _type,
     _updatedAt,
     slug
-  }`)
+  }`);
 
-  const postEntries = posts.map((post) => ({
-    url: `https://www.violatourspk.com/${post.slug.current}`,
-    lastModified: post._createdAt
-  }))
+  const postEntries = posts.map((post) => {
+    let basePath = '';
 
-  return postEntries
-}
+    if (post._type === 'tour') basePath = 'tours';
+    else if (post._type === 'treks') basePath = 'trekking';
+    else if (post._type === 'blogs') basePath = 'blogs';
 
-export default sitemap
+    return {
+      url: `https://www.violatourspk.com/${basePath}/${post.slug.current}`,
+      lastModified: post._updatedAt, // You had `_createdAt` mistakenly
+    };
+  });
+
+  return postEntries;
+};
+
+export default sitemap;
