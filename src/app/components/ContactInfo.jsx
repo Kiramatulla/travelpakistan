9539366bfe7ef231fd80227ba290040d058da7ef
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-06-28 15:59:53 +0500

    address added

--- src/app/components/ContactInfo.jsx
+++ src/app/components/ContactInfo.jsx
@@ -24,7 +24,7 @@ const ContactInfo = () => {
                 Visit Us
               </h3>
               <p className="text-sm text-gray-600 md:text-center lg:text-start">
-                Gulmit Gojal Hunza, 56000, Pakistan
+                Aliabad Hunza, 56000, Pakistan
               </p>
             </div>
           </div>
