c0dbc49da341bd1fa38fe34ea127abe5cd6282b9
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-07-23 10:32:46 +0500

    changed the font of itinerary

--- src/app/components/tourDetailPage/Itinerary.jsx
+++ src/app/components/tourDetailPage/Itinerary.jsx
@@ -1,20 +1,22 @@
 import { PortableText } from "next-sanity";
 
 const PortableTextComponents = {
-
   block: {
     h1: ({ children }) => (
       <h1 className="text-4xl font-bold mt-6 mb-2">{children}</h1>
     ),
     h2: ({ children }) => (
-      <h2 className="font-serif text-gray-800 underline underline-offset-8 text-xl rounded font-bold pl-2 lg:pl-0">{children}</h2>
+      <h2 className="text-gray-800 underline underline-offset-8 text-lg rounded pl-2 lg:pl-0">
+        {children}
+      </h2>
     ),
     h3: ({ children }) => (
-      <h3 className="text-base font-serif text-green-900 mt-5 mb-1">{children}</h3>
+      <h3 className="text-base text-green-900 mt-5 mb-1">{children}</h3>
     ),
     normal: ({ children }) => (
       <div>
-      <p className="mb-4 ">{children}</p></div>
+        <p className="mb-4 ">{children}</p>
+      </div>
     ),
   },
   list: {
@@ -26,22 +28,19 @@ const PortableTextComponents = {
     ),
   },
   listItem: {
-    bullet: ({ children }) => (
-      <li className="mb-1">{children}</li>
-    ),
-    number: ({ children }) => (
-      <li className="mb-1">{children}</li>
-    ),
+    bullet: ({ children }) => <li className="mb-1">{children}</li>,
+    number: ({ children }) => <li className="mb-1">{children}</li>,
   },
 };
 
 const Itinerary = ({ tours }) => {
-
   return (
     <article className="mx-2 lg:mx-0 lg:pl-8 lg:pr-8">
-
       <section className="font-sans text-justify lg:text-start px-4 lg:px-0 lg:text-sm lg:leading-6">
-        <PortableText value={tours.itinerary} components={PortableTextComponents}/>
+        <PortableText
+          value={tours.itinerary}
+          components={PortableTextComponents}
+        />
       </section>
     </article>
   );
