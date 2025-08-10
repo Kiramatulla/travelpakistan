b6ca8096903063a9b4aca99ad7b741f38c7c45c2
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-08-10 20:59:53 +0500

    text edited, itinerary

--- src/app/components/tourDetailPage/Itinerary.jsx
+++ src/app/components/tourDetailPage/Itinerary.jsx
@@ -36,7 +36,7 @@ const PortableTextComponents = {
 const Itinerary = ({ tours }) => {
   return (
     <article className="mx-2 lg:mx-0 lg:pl-8 lg:pr-8">
-      <section className="font-sans text-justify lg:text-start px-4 lg:px-0 lg:text-sm lg:leading-6">
+      <section className="font-sans text-sm text-justify lg:text-start px-4 lg:px-0 lg:text-sm lg:leading-6">
         <PortableText
           value={tours.itinerary}
           components={PortableTextComponents}
