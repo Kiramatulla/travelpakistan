a433b64277f1987d2a36d308ef808f210ab83c34
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-08-10 16:47:53 +0500

    photo galley edited

--- src/app/components/tourDetailPage/TourGallery.jsx
+++ src/app/components/tourDetailPage/TourGallery.jsx
@@ -6,7 +6,9 @@ export default function TourGallery({ tours }) {
 
   return (
     <section className="py-10">
-      <h2 className="text-2xl font-bold text-center mb-8">Tour Gallery</h2>
+      <h2 className="text-lg font-bold text-center mb-8">
+        Photos from my Personal Collections
+      </h2>
 
       <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
         {tours.gallery.map((item, index) => (
@@ -18,9 +20,9 @@ export default function TourGallery({ tours }) {
               <Image
                 src={urlFor(item.image).url()}
                 alt={item.caption || "Tour photo"}
-                width={200}
-                height={80}
-                className="object-cover"
+                width={300}
+                height={300}
+                className="object-cover w-[199px] h-[137px]"
               />
             </div>
             <p className="mt-2 text-sm text-center text-gray-700 font-medium">
