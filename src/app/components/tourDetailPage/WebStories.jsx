c84f48577f1a780121c52cd805cda20525cc2bf2
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-09-12 15:05:07 +0500

    added font

--- src/app/components/tourDetailPage/WebStories.jsx
+++ src/app/components/tourDetailPage/WebStories.jsx
@@ -6,7 +6,7 @@ export default function WebStories({ tours }) {
 
   return (
     <section className="mt-8">
-      <h2 className="text-2xl md:text-xl md:font-semibold text-slate-900 font-serif mb-4 pl-4">
+      <h2 className="text-2xl text-center md:text-xl md:font-semibold text-slate-900 font-serif mb-4">
         Shorts from {tours.title}
       </h2>
       <div className="flex overflow-x-auto snap-x snap-mandatory pl-1 gap-2">
