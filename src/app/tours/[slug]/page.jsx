8c5b0c4b42a10c08d10ab1a929f4a3ba948fd2de
Kiramat Ullah <148655986+Kiramatulla@users.noreply.github.com>
2025-10-03 21:35:23 +0500

    webSite error resolved

--- src/app/tours/[slug]/page.jsx
+++ src/app/tours/[slug]/page.jsx
@@ -197,7 +197,7 @@ slides[]{
       "Top Pakistan tour packages to Hunza, Skardu, Fairy Meadows & more.",
     potentialAction: {
       "@type": "SearchAction",
-      target: "https://https://www.violatourspk.com/?s={search_term_string}",
+      target: "https://www.violatourspk.com/?s={search_term_string}",
       "query-input": "required name=search_term_string",
     },
   };
